5e3084133bc630405e2a8c9d170bd20b951e5cc1
Clean <cjdrewes@gmail.com>
2023-03-01 05:55:06 -0700

    Firebase demo code

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,11 +1,27 @@
 import './App.css';
 import React from "react";
 import 'firebase/firestore';
+import FirebaseFirestoreDEMO from'./FirebaseFirestoreDEMO'
 
 class TestComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {Text: null};
         this.state = {testVar: "The value of the test variable is this!!"}
+
+        this.handleChangeText = this.handleChangeText.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChangeText(event) {
+        this.setState({Text: event.target.value})
+    }
+
+    // handle  the submission of the form
+    async handleSubmit(event) {
+        event.preventDefault();
+        await FirebaseFirestoreDEMO.addToExistingDocument(this.state.text);
+        await FirebaseFirestoreDEMO.addToNewDocument(this.state.text);
     }
 
     render() {
@@ -14,7 +30,14 @@ class TestComponent extends React.Component {
                 <h1>
                     You should only see this when in /test
                 </h1>
-                {this.state.testVar}
+                <h4>{this.state.testVar}</h4>
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text"  value={this.state.text} onChange={this.handleChangeText} placeholder="Please enter some text!"/>
+                        <div/>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
         )
     }
