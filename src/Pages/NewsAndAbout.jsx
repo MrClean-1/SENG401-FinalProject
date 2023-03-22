45993e32d2380c8e366268607d222acae7b7b9c7
Michele Pham <michele_pham@hotmail.ca>
2023-03-22 11:26:52 -0600

    put all garden page components in a panel so its pretty

--- src/Pages/NewsAndAbout.jsx
+++ src/Pages/NewsAndAbout.jsx
@@ -3,11 +3,6 @@ import React from "react";
 import 'firebase/firestore';
 
 class NewsAndAbout extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {testVar: "You should see this page when you're logged in"}
-    // }
-
     render() {
         return(
             <div style = {{textAlign: "center"}}>
